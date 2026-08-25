e80e7247de44e28b7713db79ef56133c62065d81
KEKACIK <arthur_0p@mail.ru>
2026-08-25 19:44:52 +0300

    LeetCode#206 Done. https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/

---
+++ 0001-1000/0001-0500/0201-0300/206. Reverse Linked List/main.go
@@ -0,0 +1,19 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var old *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = old
+		old = cur
+		cur = next
+	}
+
+	return old
+}
